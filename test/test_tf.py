6429c6967770fb9fe2a5a73d0ed97ebeeb0b5001
Marck Oemar <marck.oemar@gmail.com>
2021-07-21 17:04:43 +0200

    fix

--- test/test_tf.py
+++
@@ -1,7 +0,0 @@
-# testinfra https://testinfra.readthedocs.io/en/latest/modules.html#testinfra.modules.docker.Docker
-
-def test_run_terraform_version(host):
-    assert host.run_expect([0], "terraform --version")
-
-def test_run_awscli_version(host):
-    assert host.run_expect([0], "aws --version")
